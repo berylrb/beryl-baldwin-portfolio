2e3a366759fdc68174e34efa9e530795ae6bfa63
Beryl B <beriebaldwin@gmail.com>
2022-06-06 10:49:51 -0400

    first commit / page setup

---
+++ js/app.js
@@ -0,0 +1,37 @@
+// 1) Create the framework for these pages:
+  // home
+  // about
+  // projects
+  // resume
+  // contact
+
+// 2) create nav bar
+
+// 3) Store cached element references
+
+// 4) initialize animations on load
+
+// 5) Define the required constants
+
+// 6) figure out the rest of what i need to do 
+
+
+
+/*-------------------------------- Constants --------------------------------*/
+
+
+
+/*-------------------------------- Variables --------------------------------*/
+
+
+
+/*------------------------ Cached Element References ------------------------*/
+
+
+
+/*----------------------------- Event Listeners -----------------------------*/
+
+
+
+/*-------------------------------- Functions --------------------------------*/
+
