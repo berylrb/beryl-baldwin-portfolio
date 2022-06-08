725d819201cc9831049e29f695a817cb05d758de
Beryl B <beriebaldwin@gmail.com>
2022-06-08 14:00:03 -0400

    add drop down download resume link, add work cards and external js page

--- js/app.js
+++ js/app.js
@@ -17,21 +17,31 @@
 
 // import 'bootstrap'; 
 
-/*-------------------------------- Constants --------------------------------*/
+
+import { projectData } from "../my-projects.js"
 
 
 
-/*-------------------------------- Variables --------------------------------*/
+
+const cardContainer = document.getElementById('card-container')
+
+let projectMarkup = projectData.map(project => 
+`
+<div class="card" style="width: 18rem;">
+  <img class="card-img-top" src="${project.pic}" alt="Screenshot of Tic-Tac-Grow mid-game.">
+  <div class="card-body">
+    <h5 class="card-title">${project.title}</h5>
+    <p class="card-text">${project.description}</p>
+    <a href=${project.github} class="btn btn-primary">Github</a>
+    <a href=${project.launch} class="btn btn-secondary">Launch</a>
+  </div>
+</div> 
+`
+  ).join('')
+
+  cardContainer.innerHTML = projectMarkup
 
 
 
-/*------------------------ Cached Element References ------------------------*/
 
-
-
-/*----------------------------- Event Listeners -----------------------------*/
-
-
-
-/*-------------------------------- Functions --------------------------------*/
 
---
+++ my-projects.js
@@ -0,0 +1,14 @@
+
+let projectData = [
+  {
+    title: 'Tic-Tac-Grow',
+    launch: 'https://beryl-tic-tac-toe.netlify.app/',
+    pic: 'assets/project-pics/tic-tac-grow.png',
+    github: 'https://github.com/berylrb/ttt-weekend',
+    description: 'A fun take on the classic game Tic-Tac-Toe. Butilt using JavaScript, HTML, & CSS'
+  }
+]
+
+export {
+  projectData
+}
