7afc33ae2cf19beb047997afdc8f5cb2e9bfb9a5
Beryl B <beriebaldwin@gmail.com>
2022-06-08 16:06:44 -0400

    fix typo in game description, work on layout for tools icons

--- my-projects.js
+++ my-projects.js
@@ -5,7 +5,7 @@ let projectData = [
     launch: 'https://beryl-tic-tac-toe.netlify.app/',
     pic: 'assets/project-pics/tic-tac-grow.png',
     github: 'https://github.com/berylrb/ttt-weekend',
-    description: 'A fun take on the classic game Tic-Tac-Toe. Butilt using JavaScript, HTML, & CSS'
+    description: 'A fun take on the classic game Tic-Tac-Toe. Built using JavaScript, HTML, & CSS'
   }
 ]
 
