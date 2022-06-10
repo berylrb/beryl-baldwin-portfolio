a3e3069e6c072f004c689e4e31f18ee8284dfd46
Beryl B <beriebaldwin@gmail.com>
2022-06-10 01:24:26 -0400

    style stuff and interaction add

--- js/app.js
+++ js/app.js
@@ -80,17 +80,3 @@ let projectMarkup = projectData.map(project =>
   cardContainer.innerHTML = projectMarkup
 }
 
-
-
-
-if (window.location.href == host + 'projects.html') {
-<div class="card" style="width: 18rem;">
-  <img class="card-img-top" src="..." alt="Card image cap">
-  <div class="card-body">
-    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-  </div>
-</div>
-
-cardContainer.innerHTML = projectMarkup
-}
-
